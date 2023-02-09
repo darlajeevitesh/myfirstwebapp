1bccc7cf1487517b02b0666a3f264099c43a98e8
darlajeevitesh <86829228+darlajeevitesh@users.noreply.github.com>
2023-02-09 17:25:00 +0530

    Addning Form Validations

--- myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TodoController {
@@ -27,16 +29,25 @@ public class TodoController {
 	}
 	
 	@RequestMapping(value="add-todo", method = RequestMethod.GET)
-	public String showNewTodoPage() {
-		List<Todo> todos = todoService.findTodoByUsername("Udemy");
+	public String showNewTodoPage(ModelMap model) {
+		//List<Todo> todos = todoService.findTodoByUsername("Udemy");
+		String name = (String)model.get("name");
+		Todo todo = new Todo(0, name,"",LocalDate.now().plusYears(1),false);
+		model.put("todo", todo);
 		return "todo";
 	}
 	
 	@RequestMapping(value="add-todo", method = RequestMethod.POST)
-	public String addNewTodo(@RequestParam(name = "username",required = false) String username,@RequestParam(name = "description" ,required = false)String description, ModelMap model) {
+	public String addNewTodo(ModelMap model,@Valid Todo todo, BindingResult result) {
+		//@RequestParam(name = "username",required = false) String username,@RequestParam(name = "description" ,required = false)String description,
 		//String name = (String)model.get("name");
 		//(name = "username",required = false)
-		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+		
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		
+		todoService.addTodo(todo.getUsername(), todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	} 
 
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,4 +1,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
 <html>
 	<head>
 		<link href = "webjars/bootstrap/5.1.3/css/bootstrap.min.css" rel ="stylesheet">
@@ -6,11 +8,14 @@
 	<body>
 		<div class = "container">
 			<h1>Enter Todo details</h1>
-			<form method="post">
-				Username : <input type = "text", name = "username", required = "required">
-				Description : <input type = "text", name = "description", required = "required">
+			<form:form method="post" modelAttribute="todo">
+				Username : <form:input type = "text"  path ="username"  required = "required"/>
+				Description : <form:input type = "text"  path ="description"  required = "required"/>
+								<form:errors path = "description" cssClass="text-warning"/>
+				<form:input type = "hidden"  path ="id"/>
+				<form:input type = "hidden"  path ="done"/>
 				<input type = "submit" class = "btn btn-success">
-			</form>
+			</form:form>
 		</div>
 		<script src = "webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
 		<script src = "webjars/jquery/3.6.0/jquery.min.js"></script>
